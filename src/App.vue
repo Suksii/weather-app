072be354e3111aa6c9057c1ab1b9becdf5a9c78d
unknown <ramovic225@gmail.com>
2024-09-20 18:42:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-blue-300">
+    <Navbar/>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="sticky top-0 shadow-xl">
+        <nav class="flex flex-col md:flex-row items-center py-4">Navbar</nav>
+    </header>
+</template>
+
+<script setup></script>
