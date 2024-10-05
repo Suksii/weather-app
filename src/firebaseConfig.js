dd26246bd84b1c3c47d724ab4974d04cb18e81a3
unknown <ramovic225@gmail.com>
2024-10-05 18:33:53 +0200

    firebase config added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC799oAOArRIYfwZDzeoVqz2ufedlZb1uQ",
+  authDomain: "weather-app-eb593.firebaseapp.com",
+  projectId: "weather-app-eb593",
+  storageBucket: "weather-app-eb593.appspot.com",
+  messagingSenderId: "894635844",
+  appId: "1:894635844:web:ed29000b2b6f331ccb5111",
+  measurementId: "G-GL6THGCVJ4"
+};
