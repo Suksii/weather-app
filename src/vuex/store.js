0a491cada7e3c7399f665e7a26e2aa6dd4710f5e
unknown <ramovic225@gmail.com>
2024-10-05 21:40:37 +0200

    fetchWeatherData created

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,7 +1,6 @@
 import { createStore } from "vuex";
 import { db } from "@/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
-
+import { collection, getDocs, doc, collection as subCollection } from "firebase/firestore";
 
 const store = createStore({
     state: {
@@ -15,12 +14,44 @@ const store = createStore({
     actions: {
         async fetchWeatherData({ commit }) {
             try {
-                const weatherCollection = collection(db, 'cities');
-                const weatherSnapshot = await getDocs(weatherCollection);
-                const weatherList = weatherSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+                const citiesCollection = collection(db, 'cities');
+                const citiesSnapshot = await getDocs(citiesCollection);
+                const citiesList = await Promise.all(
+                    citiesSnapshot.docs.map(async cityDoc => {
+                        const cityData = { id: cityDoc.id, ...cityDoc.data() };
 
-                console.log('Fetched Weather Data:', weatherList);
-                commit('setWeatherData', weatherList)
+                        const hourlyWeatherCollection = subCollection(db, `cities/${cityDoc.id}/hourly`);
+                        const hourlyWeatherSnapshot = await getDocs(hourlyWeatherCollection);
+
+                        const hourlyWeather = await Promise.all(
+                            hourlyWeatherSnapshot.docs.map(async hourDoc => {
+                                const hourData = { hour: hourDoc.id, ...hourDoc.data() };
+
+                                const hourDetailsCollection = subCollection(db, `cities/${cityDoc.id}/hourly/${hourDoc.id}/details`);
+                                const hourDetailsSnapshot = await getDocs(hourDetailsCollection);
+
+                                const hourDetails = hourDetailsSnapshot.docs.map(detailDoc => ({
+                                    id: detailDoc.id,
+                                    ...detailDoc.data()
+                                }));
+
+                                return {
+                                    ...hourData,
+                                    details: hourDetails
+                                };
+                            })
+                        );
+
+                        return {
+                            ...cityData,
+                            hourlyWeather
+                        };
+                    })
+                );
+
+                console.log('Cities with weather:', citiesList);
+
+                commit('setWeatherData', citiesList);
             } catch (error) {
                 console.error('Error fetching data', error);
             }
@@ -29,6 +60,6 @@ const store = createStore({
     getters: {
         weatherData: (state) => state.weatherData,
     }
-})
+});
 
 export default store;
