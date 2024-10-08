fdd1c774792afa56227c8af9070b511539d7133d
unknown <ramovic225@gmail.com>
2024-10-08 11:58:06 +0200

    slider component added

---
+++ src/components/Slider.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex items-center py-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            @click="previousMonth" class="h-6 w-6 cursor-pointer text-blue-800 hover:text-blue-500 duration-300">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        <div class="relative flex overflow-hidden w-fit justify-center">
+            <div class="flex transition-transform duration-500 ease-in-out"
+                :style="{ transform: `translateX(${-100 * currentMonthIndex}%)` }">
+                <div v-for="(month, index) in months" :key="index" class="flex-shrink-0 w-full h-full text-center">
+                    <p class="text-lg text-blue-800 font-semibold">{{ month }}</p>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            @click="nextMonth" class="h-6 w-6 cursor-pointer text-blue-800 hover:text-blue-500 duration-300">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+    </div>
+</template>
+
+<script setup>
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+import { ref } from 'vue';
+
+const currentMonthIndex = ref(new Date().getMonth());
+
+const previousMonth = () => {
+    if (currentMonthIndex.value === 0) {
+        currentMonthIndex.value = months.length - 1;
+    } else {
+        currentMonthIndex.value--;
+    }
+};
+
+const nextMonth = () => {
+    if (currentMonthIndex.value === months.length - 1) {
+        currentMonthIndex.value = 0;
+    } else {
+        currentMonthIndex.value++;
+    }
+}
+</script>
--- src/views/MonthlyView.vue
+++ src/views/MonthlyView.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="w-[95%] md:w-[80%] mx-auto grid grid-cols-7 gap-4 py-10">
-        <div v-for="item in monthlyWeather" :key="item.month"
-            class=" bg-gradient-to-b from-blue-200 via-blue-100 to-blue-50 shadow-md rounded-lg transform transition duration-300 hover:scale-105 hover:shadow-lg flex flex-col justify-center items-center p-6 cursor-pointer">
-            <p class="text-lg font-semibold text-gray-800">{{ item.month }}</p>
-            <!-- <img :src="item.weather" class="h-10 w-10"/> -->
-             <p>{{ item.weather }}</p>
-            <div class="flex flex-col items-center">
-                <p class="text-2xl font-bold text-blue-600">{{ item.max_temperature }}</p>
-                <p class="text-lg text-gray-700">{{ item.min_temperature }}</p>
+    <div class="flex flex-col justify-center items-center">
+        <Slider />
+        <div class="w-[95%] md:w-[80%] mx-auto grid grid-cols-7 gap-4 py-10">
+            <div v-for="item in monthlyWeather" :key="item.month"
+                class=" bg-gradient-to-b from-blue-200 via-blue-100 to-blue-50 shadow-md rounded-lg transform transition duration-300 hover:scale-105 hover:shadow-lg flex flex-col justify-center items-center p-6 cursor-pointer">
+                <p class="text-lg font-semibold text-gray-800">{{ item.month }}</p>
+                <!-- <img :src="item.weather" class="h-10 w-10"/> -->
+                <p>{{ item.weather }}</p>
+                <div class="flex flex-col items-center">
+                    <p class="text-2xl font-bold text-blue-600">{{ item.max_temperature }}</p>
+                    <p class="text-lg text-gray-700">{{ item.min_temperature }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -17,6 +20,7 @@
 
 import { useStore } from "vuex"
 import { computed } from "vue"
+import Slider from "@/components/Slider.vue";
 
 const store = useStore();
 
@@ -25,8 +29,8 @@ const weatherData = computed(() => store.getters.weatherData);
 
 const monthlyWeather = computed(() => {
     const cityWeather = weatherData.value.find(city => city.id === selectedCity.value);
-    
+
     return cityWeather ? cityWeather.monthlyWeather[0].monthsWeather[1].daysWeather : [];
-    })
+})
 
 </script>
