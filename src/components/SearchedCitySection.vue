7b19cd14c6119d8c4e9083b476250054183ce12c
unknown <ramovic225@gmail.com>
2024-10-08 09:21:39 +0200

    searched city section modified

--- src/components/SearchedCitySection.vue
+++ src/components/SearchedCitySection.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="py-4 bg-blue-900">
-        <div class="flex items-center justify-center gap-2">
-            <img :src="Sunny" class="h-6 w-6" />
-            <span class="text-blue-300 text-lg">{{ currentWeather.temperature }}°C</span>
+        <div class="flex items-center justify-center gap-2" v-if="currentWeather">
+            <img :src="Sunny" class="h-6 w-6" v-if="currentWeather.weather"/>
+            <span v-if="currentWeather.temperature" class="text-blue-300 text-lg">{{ currentWeather.temperature }}°C</span>
             <span class="text-blue-300 text-lg">{{ selectedCity }}</span>
+        </div>
+        <div v-else class="flex items-center justify-center gap-2">
+           <p class="text-blue-300 tracking-wide text-lg">Weather data will be available soon...</p>
         </div>
     </div>
 </template>
@@ -18,8 +21,6 @@ const store = useStore();
 const selectedCity = computed(() => store.getters.selectedCity);
 
 const currentWeather = computed(() => store.getters.currentWeather)
-
-console.log(selectedCity, currentWeather);
 
 
 
