a744e087e43e8292353a936b70a7507405eceea4
unknown <ramovic225@gmail.com>
2024-10-10 16:21:31 +0200

    footer modified

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,10 +1,14 @@
 <template>
     <footer class="text-blue-50 pt-4 pb-2 w-full">
         <div class="max-w-screen-xl mx-auto text-center">
-            <p class="text-lg font-semibold">&copy; 2024 Weather Channel Montenegro</p>
+            <p class="text-lg font-semibold">&copy; {{ currentYear }} Weather Channel Montenegro</p>
             <p class="text-sm mt-2">All rights reserved</p>
         </div>
     </footer>
 </template>
 
-<script setup></script>
+<script setup>
+
+const currentYear = new Date().getFullYear();
+
+</script>
